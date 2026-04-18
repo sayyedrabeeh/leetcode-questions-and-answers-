25c7a778bf0f62e579978a05a92053723c76f049
Sayyed Rabeeh <sayyedrabeeh240@gmail.com>
2026-04-18 16:34:18 +0530

    1662-check-if-two-string-arrays-are-equivalent

--- 1662-check-if-two-string-arrays-are-equivalent/1662-check-if-two-string-arrays-are-equivalent.py
+++ 1662-check-if-two-string-arrays-are-equivalent/1662-check-if-two-string-arrays-are-equivalent.py
@@ -1,6 +1,9 @@
 class Solution:
     def arrayStringsAreEqual(self, word1: List[str], word2: List[str]) -> bool:
-        w1 = ''.join(word1)
-        w2 = ''.join(word2)
+        w1 = ''
+        w2 = ''
+        for ch in word1:
+            w1+=ch
+        for ch in word2:
+            w2+=ch
         return w1 == w2
-    
